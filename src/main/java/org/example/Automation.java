8ab6ba6280eaa987074abef93b24916d7332b5c6
Omar Ibrahim <Omar.ibrahim@github.com>
2024-10-20 17:05:46 +0300

    first try

---
+++ src/main/java/org/example/Automation.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automation {
+
+    public static void main(String[] args) {
+
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/");
+
+
+        WebElement UserName =driver.findElement(By.partialLinkText("event")) ;
+
+        UserName.click();
+
+
+
+
+
+
+//        driver.quit();
+
+
+
+
+
+
+
+
+
+
+    }
+}
