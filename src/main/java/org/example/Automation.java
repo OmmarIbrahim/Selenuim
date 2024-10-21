e115862f5fa6c4b46afb5e99988a1469f616bb40
Omar Ibrahim <Omar.ibrahim@github.com>
2024-10-21 16:34:16 +0300

    new features

--- src/main/java/org/example/Automation.java
+++ src/main/java/org/example/Automation.java
@@ -1,33 +1,16 @@
 package org.example;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Automation {
 
-    //public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
         WebDriver driver = new ChromeDriver();
 
-      //  driver.get("https://the-internet.herokuapp.com/");
-
-
-        WebElement UserName =driver.findElement(By.partialLinkText("event")) ;
-
-      //  UserName.click();
-
-
-
-
-
-
-
-
-//        driver.quit();
 
 
 
@@ -38,5 +21,47 @@ public class Automation {
 
 
 
-  //  }
+
+
+
+    /*  driver.get("https://duckduckgo.com/");
+
+
+        driver.manage().window().maximize();
+
+
+      WebElement Search = driver.findElement(By.name("q"));
+
+      Search.sendKeys("panda");
+
+        WebElement searchButton = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        searchButton.click();
+
+
+
+      WebElement x=   driver.findElement(By.xpath("//a[@href='/?t=h_&q=panda&ia=images&iax=images']"));
+
+x.click();
+
+WebElement z =      driver.findElement(By.xpath("//a[@href='/?t=h_&q=panda&ia=videos&iax=videos']"));
+
+         z.click();
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
--- src/main/java/org/example/FindElementByID.java
+++ src/main/java/org/example/FindElementByID.java
@@ -1,7 +1,51 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.sleep;
+
 public class FindElementByID {
 
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://duckduckgo.com/");
+
+        WebElement Search_input = driver.findElement(By.id("searchbox_input"));
+
+        Search_input.sendKeys("Panda");
+        //here we couldn't find an id the element, so I decide to use the Xpath
+
+        WebElement Click = driver.findElement(By.xpath("//button[@aria-label='Search']"));
+
+
+        Click.click();
+
+        try {
+            // Pausing execution for 5 seconds (5000 milliseconds)
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.navigate().back();
+
+        Search_input.clear();
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 }
